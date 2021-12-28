5aa2189d7ab797ad20f3687cca92e9075f79d39d
hedwig100 <pentagon7951@outlook.jp>
2021-12-28 15:40:11 +0900

    [update] add some test case

--- internal/route/route_tasks.go
+++ internal/route/route_tasks.go
@@ -1,1 +1,29 @@
 package route
+
+import (
+	"net/http"
+)
+
+// /task-lists
+// POST
+func taskListsCreate(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+// /task-lists/{listId}
+// GET
+func taskListsGet(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+// /task-lists/{listId}
+// PUT
+func taskListsUpdate(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+// /task-lists/{listId}
+// DELETE
+func taskListsDelete(writer http.ResponseWriter, request *http.Request) {
+
+}
--- internal/route/route_test.go
+++ internal/route/route_test.go
@@ -1,15 +1,21 @@
 package route
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"strings"
 	"testing"
+
+	"github.com/hedwig100/todo-app/internal/data"
 )
 
 var mux http.ServeMux
 var writer *httptest.ResponseRecorder
+
+var createdUsername = []string{"hedwig100", "pokemon", "mac"}
+var createdPassword = []string{"iajgo3o", ")8hgiau", "uhaig1928"}
 
 // REVIEW:よりよいテストの仕方,依存性の注入?
 func TestMain(m *testing.M) {
@@ -21,10 +27,18 @@ func TestMain(m *testing.M) {
 func setUp() {
 	mux = GetMux()
 	writer = httptest.NewRecorder()
+
+	// データを消す
+	data.Db.Exec("DELETE FROM users") // REVIEW:設計としてこういうのはよくないかも?
+
+	// テストデータを挿入する
+	for index, username := range createdUsername {
+		data.UserCreate(username, createdPassword[index])
+	}
 }
 
 func TestCreateUser(t *testing.T) {
-	// usernameを変えないとusernameの重複を許さないためテストが失敗する
+	// 名前が被らないならユーザを作成できる
 	json := strings.NewReader(`{
 		"username":"fagge9092",
 		"password":"98h9ghiafe"
@@ -40,22 +54,31 @@ func TestCreateUser(t *testing.T) {
 		errMsg := writer.Body.String()
 		t.Error(errMsg)
 	}
+
+	// 名前が同じならユーザは作成できない
+	json = strings.NewReader(fmt.Sprintf(`{
+		"username": "%s", 
+		"password":"9g8aho3"
+	}`, createdUsername[0]))
+	request, err = http.NewRequest("POST", "/users", json)
+	if err != nil {
+		t.Error(err)
+	}
+	writer = httptest.NewRecorder()
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 500 {
+		t.Error("can create user although this name is already used")
+	}
 }
 
 func TestDeleteUser(t *testing.T) {
-	json := strings.NewReader(`{
-		"username":"fagfa92",
-		"password":"98hfaiafe"
-	}`)
-	request, _ := http.NewRequest("POST", "/users", json)
-	writer = httptest.NewRecorder() // REVIEW:NewRecorderは生成しなくていいはず、bufferを消すとかで使いまわせそう
-	mux.ServeHTTP(writer, request)
-
-	json = strings.NewReader(`{
-		"username":"fagfa92",
-		"password":"98hfaiafe"
-	}`)
-	request, _ = http.NewRequest("DELETE", "/users", json)
+	// 存在するユーザがdeleteできる
+	json := strings.NewReader(fmt.Sprintf(`{
+		"username":"%s",
+		"password":"%s"
+	}`, createdUsername[1], createdPassword[1]))
+	request, _ := http.NewRequest("DELETE", "/users", json)
 	writer = httptest.NewRecorder()
 	mux.ServeHTTP(writer, request)
 	if writer.Code != 200 {
@@ -63,23 +86,41 @@ func TestDeleteUser(t *testing.T) {
 		errMsg := writer.Body.String()
 		t.Error(errMsg)
 	}
-}
 
-func TestLogin(t *testing.T) {
-	json := strings.NewReader(`{
-		"username":"faoj",
-		"password":"9faade"
-	}`)
-	request, _ := http.NewRequest("POST", "/users", json)
+	// 存在するがpasswordが違う場合はdeleteできない
+	json = strings.NewReader(fmt.Sprintf(`{
+		"username":"%s",
+		"password":"%s"
+	}`, createdUsername[1], createdPassword[0]))
+	request, _ = http.NewRequest("DELETE", "/users", json)
 	writer = httptest.NewRecorder()
 	mux.ServeHTTP(writer, request)
 
-	// ログインできること
+	if writer.Code != 500 {
+		t.Error("can delete user although password is not correct")
+	}
+
+	// 存在しないuserを消そうとするとエラー
 	json = strings.NewReader(`{
-		"username":"faoj",
-		"password":"9faade"
+		"username":"982ni",
+		"password":"fahi"
 	}`)
-	request, _ = http.NewRequest("POST", "/users/login", json)
+	request, _ = http.NewRequest("DELETE", "/users", json)
+	writer = httptest.NewRecorder()
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 500 {
+		t.Error("can delete user although the user is not created")
+	}
+}
+
+func TestLogin(t *testing.T) {
+	// 存在するユーザにログインできること
+	json := strings.NewReader(fmt.Sprintf(`{
+		"username":"%s",
+		"password":"%s"
+	}`, createdUsername[2], createdPassword[2]))
+	request, _ := http.NewRequest("POST", "/users/login", json)
 	writer = httptest.NewRecorder()
 	mux.ServeHTTP(writer, request)
 	if writer.Code != 201 {
@@ -89,14 +130,42 @@ func TestLogin(t *testing.T) {
 	}
 
 	// passwordが違うとログインできないこと
-	json = strings.NewReader(`{
-		"username":"faoj",
-		"password":"ohgia"
-	}`)
+	json = strings.NewReader(fmt.Sprintf(`{
+		"username":"%s",
+		"password":"%s"
+	}`, createdUsername[2], createdPassword[1]))
 	request, _ = http.NewRequest("POST", "/users/login", json)
 	writer = httptest.NewRecorder()
 	mux.ServeHTTP(writer, request)
 	if writer.Code != 500 {
 		t.Fatal("can login although password is not correct")
 	}
+
+	// 存在しないユーザにログインできないこと
+	json = strings.NewReader(`{
+		"username":"gea",
+		"password":"og0983a"
+	}`)
+	request, _ = http.NewRequest("POST", "/users/login", json)
+	writer = httptest.NewRecorder()
+	mux.ServeHTTP(writer, request)
+	if writer.Code != 500 {
+		t.Fatal("can login although the user is not created")
+	}
+}
+
+func TestCreateTaskList(t *testing.T) {
+	t.Skip()
+}
+
+func TestGetTaskList(t *testing.T) {
+	t.Skip()
+}
+
+func TestUpdateTaskList(t *testing.T) {
+	t.Skip()
+}
+
+func TestDeleteTaskList(t *testing.T) {
+	t.Skip()
 }
